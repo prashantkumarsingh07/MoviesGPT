54d089711b72541a6c2b074d3e5063f1a9477de2
Prashant Kumar Singh <prashantkumarsingh07pks@gmail.com>
2024-03-01 17:24:51 +0530

    Created movie suggestion list and moviecards.

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,9 +1,20 @@
-import React from 'react'
+
+import MovieList from './MovieList';
+import { useSelector } from 'react-redux';
 
 const SecondaryContainer = () => {
+  const movies = useSelector((store)=>store.movies);
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    movies.nowPlayingMovies && (<div className='  bg-black'>
+    <div className=' -mt-56 pl-12   relative z-20 '>
+    <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+    <MovieList title={"Trending Movies"} movies={movies.nowPlayingMovies}/>
+    <MovieList title={"Popular Movies"} movies={movies.nowPlayingMovies}/>
+    <MovieList title={"Horror Movies"} movies={movies.nowPlayingMovies}/>
+    </div>
+    
+    </div>)
+  );
+};
 
 export default SecondaryContainer;
