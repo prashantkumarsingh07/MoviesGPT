6e9b9f760a91ee41faae61916a29e290f0305c94
Prashant Kumar Singh <prashantkumarsingh07pks@gmail.com>
2024-03-01 12:33:00 +0530

    created custom hook for trailer video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,35 +1,13 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+
+import {  useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+
 
 
 const VideoBackground = ({movieId}) => {
   const trailerVideo =useSelector(store=>store.movies?.trailerVideo);
-  const dispatch = useDispatch();
   
-
-  // fetch trailer video  
-   
-  const getMovieVideo = async () =>{
-     
-    const data = await fetch('https://api.themoviedb.org/3/movie/1096197/videos?language=en-US', API_OPTIONS);
-     
-    const json= await data.json();
-    
-    console.log(json);
-
-    const filterData = json.results.filter(video=>video.type==="Trailer");
-    const trailer = filterData.length?filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(()=>{
-    getMovieVideo();
-  },[]);
-
-  
+  useMovieTrailer(movieId);
 
   return (
     <div><iframe width="560" height="315" src={"https://www.youtube.com/embed/e1k1PC0TtmE?si=SlyQLIGnC4a-9-Px"+trailerVideo?.key} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -13,7 +13,7 @@ const useNowPlayingMovies =()=>{
     const data= await fetch( 'https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
   
     const json =await data.json();
-    console.log(json.results);
+   
     dispatch(addNowPlayingMovies(json.results));
   };
   
