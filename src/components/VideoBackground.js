a5f6f2f781fe0f656d5344dc0baa1017cfbfe5d7
Prashant Kumar Singh <prashantkumarsingh07pks@gmail.com>
2024-03-01 11:26:23 +0530

    Building VideoBackground

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,10 +1,38 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
 
 const VideoBackground = ({movieId}) => {
+  const trailerVideo =useSelector(store=>store.movies?.trailerVideo);
+  const dispatch = useDispatch();
+  
 
-  // fetch trailer video
+  // fetch trailer video  
+   
+  const getMovieVideo = async () =>{
+     
+    const data = await fetch('https://api.themoviedb.org/3/movie/1096197/videos?language=en-US', API_OPTIONS);
+     
+    const json= await data.json();
+    
+    console.log(json);
+
+    const filterData = json.results.filter(video=>video.type==="Trailer");
+    const trailer = filterData.length?filterData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(()=>{
+    getMovieVideo();
+  },[]);
+
+  
+
   return (
-    <div>VideoBackground</div>
+    <div><iframe width="560" height="315" src={"https://www.youtube.com/embed/e1k1PC0TtmE?si=SlyQLIGnC4a-9-Px"+trailerVideo?.key} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
   )
 } 
 
