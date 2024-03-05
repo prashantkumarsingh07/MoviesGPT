68dcaa4fdc1a3eff8d40a3f9ee678599870e4e35
Prashant Kumar Singh <prashantkumarsingh07pks@gmail.com>
2024-03-05 21:00:30 +0530

    using chatgpt api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,5 @@ export const API_OPTIONS={
 export const IMG_CDN_URL ="https://image.tmdb.org/t/p/w500";
 
 export const SUPPORTED_LANGUAGES =[{identifier: "en", name: "English"},{identifier: "hindi", name: "Hindi"} , {identifier: "spanish", name: "Spanish"} ];
+
+export const OPENAI_KEY= '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';
